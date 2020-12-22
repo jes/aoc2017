dc31d59fc23303f1541e694a5d2ec35ff9c64aa7
James Stanley <james@incoherency.co.uk>
2020-12-22 11:39:39 +0000

    4

---
+++ 4.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use List::Util qw(any);
+
+my $n = 0;
+
+while (<>) {
+    chomp;
+
+    my @w = split / /;
+    my %c;
+    $c{$_}++ for @w;
+
+    $n++ unless any { $c{$_} > 1 } keys %c;
+}
+
+print "$n\n";
